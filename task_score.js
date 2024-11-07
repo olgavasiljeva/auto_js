aded873fd41a60c91473447db6af3724c6964982
Olga <alaninmg@gmail.com>
2024-11-07 23:23:08 +0200

    HM-2-1-3

---
+++ task_score.js
@@ -0,0 +1,16 @@
+//расчет среднего балла за домашние задания студента по модулям
+
+const introScore = 50;
+const gitScore = 79;
+const jsScore = 65;
+
+let total = introScore + gitScore + jsScore; //вычисление общей суммы баллов по модулям
+let averageScore = total / 4 //вычисление среднего значения баллов по модулям
+
+
+const firstName = "Mike"
+const lastName = "Smith"
+
+let studentName = firstName + " " +lastName;
+
+console.log(`The average score for student ${studentName} for the three modules will be ${averageScore}.`);
