8031a41db42f707295eb57d26f896c87cd47a6f4
Olga <alaninmg@gmail.com>
2025-09-23 23:26:53 +0300

    edited a task

--- task_score.js
+++ task_score.js
@@ -4,9 +4,7 @@ const introScore = 50;
 const gitScore = 79;
 const jsScore = 65;
 
-let total = introScore + gitScore + jsScore; //вычисление общей суммы баллов по модулям
-let averageScore = total / 4 //вычисление среднего значения баллов по модулям
-
+let averageScore = (introScore + gitScore + jsScore) / 3; //вычисление общей суммы баллов по модулям и вычисление среднего балла
 
 const firstName = "Mike"
 const lastName = "Smith"
